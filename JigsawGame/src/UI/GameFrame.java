ed2f8886be55fcdcd14bbea1278ea75448158256
泽知夏 <17525305+Midnight_Su@user.noreply.gitee.com>
2026-09-10 18:28:45 +0800

    添加背景、边框，实现移动

--- JigsawGame/src/UI/GameFrame.java
+++ JigsawGame/src/UI/GameFrame.java
@@ -1,11 +1,19 @@
 package UI;
 
 import javax.swing.*;
+import javax.swing.border.BevelBorder;
+import java.awt.*;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.Random;
 
-public class GameFrame extends JFrame {
+public class GameFrame extends JFrame implements KeyListener {
     //创建一个二维数组，用于存储图片序号
     int[][] date = new int[4][4];
+    //定义两个变量，用于记录空白块的位置
+    int x = 0;
+    int y = 0;
 
     public GameFrame() {
         //初始化游戏窗口
@@ -38,6 +46,8 @@ public class GameFrame extends JFrame {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         //设置窗口布局为null，取消默认居中放置，手动设置组件位置
         this.setLayout(null);
+        //添加键盘事件监听器
+        this.addKeyListener(this);
     }
 
     private void initJMenuBar() {
@@ -48,20 +58,20 @@ public class GameFrame extends JFrame {
         //添加菜单栏到窗口
         this.setJMenuBar(jMenuBar);
 
-        //创建菜单栏选项（功能、关于我们）
+        //创建菜单栏选项（功能、关于）
         JMenu functionMenu = new JMenu("功能");
-        JMenu aboutMenu = new JMenu("关于我们");
+        JMenu aboutMenu = new JMenu("关于");
 
         //添加菜单栏选项到菜单栏
         jMenuBar.add(functionMenu);
         jMenuBar.add(aboutMenu);
 
-        //创建菜单栏选项的条目（功能：重新游戏、重新登录、退出游戏;关于我们：联系方式）
+        //创建菜单栏选项的条目（功能：重新游戏、重新登录、退出游戏;关于：联系）
         JMenuItem replayItem = new JMenuItem("重新游戏");
         JMenuItem reloginItem = new JMenuItem("重新登录");
         JMenuItem exitItem = new JMenuItem("退出游戏");
 
-        JMenuItem contactItem = new JMenuItem("联系方式");
+        JMenuItem contactItem = new JMenuItem("联系");
 
         //添加菜单栏选项的条目到菜单栏选项
         functionMenu.add(replayItem);
@@ -86,6 +96,11 @@ public class GameFrame extends JFrame {
         //给二维数组赋值
         for(int i = 0; i<4; i++){
             for (int j = 0; j<4; j++){
+                //记录数组元素16即空白块的角标
+                if(temArr[i*4+j] == 16){
+                    x = i;
+                    y = j;
+                }
                 date[i][j] = temArr[i*4+j];
             }
         }
@@ -102,6 +117,10 @@ public class GameFrame extends JFrame {
         //this.add(imageLabel);
         //this.getContentPane().add(imageLabel01);
 
+        //清空原本的图片
+        this.getContentPane().removeAll();
+
+        //加载图片
         for(int i = 0; i<4; i++){
             for (int j = 0; j<4; j++){
                 //获取当前图片序号
@@ -109,15 +128,107 @@ public class GameFrame extends JFrame {
 
                 //根据图片序号加载图片
                 if(number < 10){
+                //加载01.jpg-09.jpg
                 JLabel imageLabel = new JLabel(new ImageIcon("JigsawGame/image/ACG/ACG01/0"+number+".jpg"));
-                imageLabel.setBounds(105*j,105*i,105,105);
+                //设置图片imageLabel的位置
+                imageLabel.setBounds(105*j+91,105*i+130,105,105);
+                //给图片添加边框
+                //RAISED = 0：凸起边框
+                //LOWERED = 1：下陷边框
+                imageLabel.setBorder(new BevelBorder(BevelBorder.RAISED));
+                //添加图片imageLabel到窗口
                 this.getContentPane().add(imageLabel);
                 }else if(number <= 15){
+                //加载10.jpg-15.jpg
                 JLabel imageLabel = new JLabel(new ImageIcon("JigsawGame/image/ACG/ACG01/"+number+".jpg"));
-                imageLabel.setBounds(105*j,105*i,105,105);
+                //设置图片imageLabel的位置
+                imageLabel.setBounds(105*j+91,105*i+130,105,105);
+                //给图片添加边框
+                //RAISED = 0：凸起边框
+                //LOWERED = 1：下陷边框
+                imageLabel.setBorder(new BevelBorder(BevelBorder.RAISED));
+                //添加图片imageLabel到窗口
                 this.getContentPane().add(imageLabel);
                 }
             }
+        }
+
+        //添加背景图片
+        JLabel background = new JLabel(new ImageIcon("JigsawGame/image/sport/background.jpg"));
+        background.setBounds(0,0,603,680);
+        this.getContentPane().add(background);
+
+        //刷新窗口
+        this.getContentPane().repaint();
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        //判断上下左右方向键是否被按下
+        //左：VK_LEFT = 37，上：VK_UP = 38，右：VK_RIGHT = 39，下：VK_DOWN = 40
+        int keyCode = e.getKeyCode();
+        if(keyCode == KeyEvent.VK_LEFT){
+            System.out.println("点击了左键");
+            //判断是否可以向左移动
+            if(y == 3){
+                return;
+            }
+            //空白块儿右方图片的数字赋值给空白块儿
+            date[x][y] = date[x][y+1];
+            date[x][y+1] = 16;
+            //更新空白块儿的角标
+            y++;
+            //刷新图片
+            initImage();
+        }else if(keyCode == KeyEvent.VK_UP){
+            System.out.println("点击了上键");
+            //判断是否可以向上移动
+            if(x == 3){
+                return;
+            }
+            //空白块儿下方图片的数字赋值给空白块儿
+            date[x][y] = date[x+1][y];
+            date[x+1][y] = 16;
+            //更新空白块儿的角标
+            x++;
+            //刷新图片
+            initImage();
+        }else if(keyCode == KeyEvent.VK_RIGHT){
+            System.out.println("点击了右键");
+            //判断是否可以向右移动
+            if(y == 0){
+                return;
+            }
+            //空白块儿左方图片的数字赋值给空白块儿
+            date[x][y] = date[x][y-1];
+            date[x][y-1] = 16;
+            //更新空白块儿的角标
+            y--;
+            //刷新图片
+            initImage();
+        }else if(keyCode == KeyEvent.VK_DOWN){
+            System.out.println("点击了下键");
+            //判断是否可以向下移动
+            if(x == 0){
+                return;
+            }
+            //空白块儿上方图片的数字赋值给空白块儿
+            date[x][y] = date[x-1][y];
+            date[x-1][y] = 16;
+            //更新空白块儿的角标
+            x--;
+            //刷新图片
+            initImage();
         }
     }
 }
--- JigsawGame/src/UI/Test.java
+++ JigsawGame/src/UI/Test.java
@@ -8,18 +8,38 @@ public class Test {
         JFrame gameFrame = new JFrame();
         gameFrame.setTitle("拼图游戏");
         gameFrame.setSize(603,680);
+        //设置界面居中显示
+        gameFrame.setLocationRelativeTo(null);
+        //设置界面关闭模式
+        gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //取消默认容器的默认居中布局
+        gameFrame.getContentPane().setLayout(null);
+
+        //创建一个按钮对象
+        JButton Button = new JButton("监听");
+        //设置按钮的位置和大小
+        Button.setBounds(100,100,100,30);
+        //给按键添加事件监听器
+        //实现ActionListener接口，重写actionPerformed方法
+        Button.addActionListener(e -> {
+            System.out.println("点击了按钮");
+        });
+        //添加按钮到窗口
+        gameFrame.getContentPane().add(Button);
+
+        //设置界面可见
         gameFrame.setVisible(true);
 
         //2.创建一个登录界面
-        JFrame loginFrame = new JFrame();
-        loginFrame.setTitle("登录");
-        loginFrame.setSize(488,430);
-        loginFrame.setVisible(true);
+        //JFrame loginFrame = new JFrame();
+        //loginFrame.setTitle("登录");
+        //loginFrame.setSize(488,430);
+        //loginFrame.setVisible(true);
 
         //3.创建一个注册界面
-        JFrame registerFrame = new JFrame();
-        registerFrame.setTitle("注册");
-        registerFrame.setSize(488,500);
-        registerFrame.setVisible(true);
+        //JFrame registerFrame = new JFrame();
+        //registerFrame.setTitle("注册");
+        //registerFrame.setSize(488,500);
+        //registerFrame.setVisible(true);
     }
 }
